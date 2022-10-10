e798033bc14f1ab19148a6bf641e9afe35a9dc40
Elfcwx <110804138+Elfcwx@users.noreply.github.com>
2022-10-10 12:46:41 +0800

    Add files via upload

---
+++ day_01_1.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+void main()
+{
+	int a, b;
+	char c;
+	printf("请输入两位数字进行+ - * /运算\n");
+	scanf("%d %c %d", &a, &c ,& b);
+	switch (c)
+	{
+	case'+':
+		printf("%d + %d = %d\n", a, b, a + b);
+		break;
+	case'-':
+		printf("%d - %d = %d\n", a, b, a - b);
+		break;
+	case'*':
+		printf("%d * %d = %d\n", a, b, a * b);
+		break;
+	case'/':
+		if (b == 0) {
+			printf("除数不能为0\n");
+		}
+		else
+		{
+			printf("%d / %d = %d\n", a, b, a / b);
+		}
+	default:
+		printf("输入方式有误，无法计算\n");
+		break;
+	}
+}
